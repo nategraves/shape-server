900f9016e007d5a9bd55d0c1bdae7d69f83f6479
Nate Graves <natesgraves@gmail.com>
2017-11-09 15:34:56 -0500

    Adding app file

---
+++ app.py
@@ -0,0 +1,53 @@
+""" Simple server that returns a path string generated from a char model RNN """
+from __future__ import division, print_function
+
+import re
+import subprocess
+from flask import Flask, jsonify
+from svgpathtools import parse_path, Path
+
+app = Flask(__name__.split('.')[0]) # pylint: disable=invalid-name
+
+def generate(max_size = 500):
+    """ Returns an SVG path as a string """
+    forever = True
+    while forever:
+        final_path = None
+        while final_path is None:
+            generate_path = ('th sample.lua -checkpoint cv/paths_60000.t7 -length 3500 '
+                '-start_text M -sample 1 -temperature 0.5').split()
+            nn_output = subprocess.check_output(generate_path).decode('utf-8')
+            reg_ex = re.compile(r'M.+Z')
+            path_strings = reg_ex.findall(nn_output) 
+            for path in path_strings:
+                p = parse_path(path)
+                if p.isclosed():
+                    xmin, xmax, ymin, ymax = p.bbox()
+                    width = xmax - xmin
+                    print(width)
+                    height = ymax - ymin
+                    print(height)
+                    print("**********************************")
+                    if width > 1 and width < max_size and height > 1 and height < max_size:
+                        final_path = p.d()
+                        break
+
+        with open('shapes.txt', 'r+') as f:
+            svgs = f.read().splitlines()
+            if final_path not in svgs:
+                f.write(path + '\n')
+                forever = False
+                break
+    return final_path
+
+
+@app.route('/')
+def index():
+    with open('shapes.txt', 'r') as f:
+        paths = [line.rstrip() for line in f.readlines()]
+        response = jsonify({ 'paths': paths })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
